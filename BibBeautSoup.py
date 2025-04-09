0d558cfe09cab871acb67ed629cb6c269386b71d
wafa <wafa.aligi@gmail.com>
2025-04-09 13:26:39 -0400

    récupérer les catégories d'un menu side récupération des sources des images d'un fichier index

--- BibBeautSoup.py
+++ BibBeautSoup.py
@@ -3,13 +3,22 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 
-url = "https://books.toscrape.com/"
-response =  requests.get(url)
+# url = "https://books.toscrape.com/"
+# response =  requests.get(url)
+#
+# with open('index.html', 'w') as f :
+#     html = f.write(response.text)
 
+with open('index.html', 'r') as f:
+    html = f.read()
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 aside = soup.find('div', class_="side_categories")
 categories_div = aside.find("ul").find('li').find('ul')
 categories = [child.text.strip() for child in categories_div.children if child.name]
 
-pprint(categories)
+images = soup.find('section').find_all('img')
+
+sources = [image.get('src') for image in images]
+
+pprint(sources)
