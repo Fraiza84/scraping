c031d806e1ebbd7ad2f12266144e511ab3617f02
wafa <wafa.aligi@gmail.com>
2025-04-09 12:51:18 -0400

    récupérer les catégories d'un menu side

---
+++ BibBeautSoup.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+url = "https://books.toscrape.com/"
+response =  requests.get(url)
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+aside = soup.find('div', class_="side_categories")
+categories_div = aside.find("ul").find('li').find('ul')
+categories = [child.text.strip() for child in categories_div.children if child.name]
+
+pprint(categories)
