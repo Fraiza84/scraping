2028e2aa64a83827556b30bcdae185b23aef8bb1
wafa <wafa.aligi@gmail.com>
2025-04-11 14:35:50 -0400

    Récuperation des catégories avec un seul livre

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-import  requests
+from urllib.parse import urljoin
 
-response = requests.get("https://www.google.com")
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
 
-with open('index.html', 'w') as f :
-    f.write(response.text)
+BASE_URL = "https://books.toscrape.com"
 
+
+def main(threshold: int=5 ):
+    with requests.Session() as session:
+
+        response = session.get(BASE_URL)
+        soup = BeautifulSoup(response.text, 'html.parser')
+       # soup.find("ul", class_= "nav nav-list").find_all("a")
+
+        liens = soup.select('ul.nav.nav-list a')
+        categories_url = [lien["href"] for lien in liens[1:]]
+
+        for category_url in categories_url:
+            full_url = urljoin(BASE_URL,category_url)
+            response = session.get(full_url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            books = soup.select("article.product_pod")
+            category = soup.select_one("h1").text
+            number_of_books = len(books)
+            if number_of_books <= threshold:
+                print(f"La catégorie {category} ne contient pas assez de livre ({len(books)})")
+
+if __name__ == '__main__':
+    main(threshold=10)
 
